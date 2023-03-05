a55add51a89a0ce2c1ced1f49cefc5b57ad57a0b
MIA <mamatalievaindira1@gmail.com>
2023-03-05 20:32:26 +0600

    Initial Commit

---
+++ practice 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice 3
+//
+//  Created by Indira on 5/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
